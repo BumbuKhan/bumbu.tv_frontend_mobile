da4153a30b37fdf6b824be0ed837f35f35e16105
Курбан Курбанов <qurban.qranov93@gmail.com>
2017-04-23 17:10:21 +0400

    Added search window (popdown)

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
 
     // hamburger menu init
-    $('nav#hamburger-menu').mmenu();
+    $('#hamburger-menu').mmenu({
+        'extensions': {
+            'all': ['theme-dark']
+        }
+    });
+
+    // search-window
+    $('#search-window').mmenu({
+        extensions: ['border-none', 'shadow-page', 'theme-white', 'pagedim-black'],
+        offCanvas: {
+            zposition: 'front',
+            position: 'top'
+        },
+        navbar: false,
+        navbars: {
+            content: ['searchfield', 'close']
+        }
+    }, {})
+        .on('click', 'a[href^="#/"]', function () {
+                alert('Thank you for clicking, but that\'s a demo link.');
+                return false;
+            }
+        );
 });
 
 // horizontal scroll
